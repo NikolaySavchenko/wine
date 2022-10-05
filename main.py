3b1bfca19e4f1d2a944f8a74474079b688b93af4
Nikolay Savchenko <7548568@gmail.com>
2022-10-05 21:14:47 +0300

    Update .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from secondary_functions import winery_age
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from secondary_functions import data_from_file
+import argparse
 
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+parser = argparse.ArgumentParser('Input .xlsx file address')
+parser.add_argument('file')
+file_name = parser.parse_args().file
+
+rendered_page = template.render(wine=data_from_file(file_name), age=winery_age())
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
