7083a4d3ca6ca6ae5aa9ff1ec1a9c80e5d47c909
Nikolay Savchenko <7548568@gmail.com>
2022-10-06 21:18:55 +0300

    Using ifmain

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from secondary_functions import winery_age, data_from_file
 import argparse
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    parser = argparse.ArgumentParser('Input Excel file address')
+    parser.add_argument('file')
+    file_address = parser.parse_args().file
+    rendered_page = template.render(wine=data_from_file(file_address), age=winery_age())
 
-parser = argparse.ArgumentParser('Input .xlsx file address')
-parser.add_argument('file')
-file_name = parser.parse_args().file
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-rendered_page = template.render(wine=data_from_file(file_name), age=winery_age())
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+if __name__ == '__main__':
+    main()
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
